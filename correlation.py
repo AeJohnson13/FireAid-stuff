593bf8f01c4a243bba9028d2efb734ae9a27af8e
Alex Johnson <aej800@gmail.com>
2025-10-07 13:12:27 -0800

    started doing correlation

---
+++ correlation.py
@@ -0,0 +1,12 @@
+# !/usr/bin/env python3
+# correlation.py
+# Alex Johnson
+# 2025-10-07
+
+import pandas as pd
+import numpy as np
+
+input_file = 'InVariables.csv'
+inVariables = pd.DataFrame()
+inVariables.to_csv(input_file,index=False)
+
