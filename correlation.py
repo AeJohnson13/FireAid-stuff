b5537a3a951f4b4953e1c6a7fbe24688ea7e65d3
Alex Johnson <112223622+AeJohnson13@users.noreply.github.com>
2025-10-27 08:22:04 -0800

    work

--- correlation.py
+++ correlation.py
@@ -6,10 +6,27 @@
 import pandas as pd
 import numpy as np
 
-input_file = 'InVariables.csv'
+input_file = "Mini Project Dataset Narrowed.csv"
+fireData = pd.DataFrame()
+fireData = pd.read_csv(input_file)
 
-inVariables = pd.DataFrame()
 
-inVariables = pd.read_csv(input_file)
+# Continuous Variables
+Acres = fireData.ESTIMATEDTOTALACRES.to_numpy()
 
-print(inVariables)
+# Discrete Ranges
+Slope = fireData.ORIGINSLOPE.to_numpy()
+Aspect = fireData.ORIGINASPECT.to_numpy()
+Elevation = fireData.ORIGINELEVATION.to_numpy()
+
+# Fuel/Vegetation Bools
+Spruce = fireData.SPRUCE.to_numpy()
+Tundra = fireData.MUNDRA.to_numpy() # moss and tundra included
+Grass = fireData.GRASS.to_numpy()
+Brush = fireData.BRUSH.to_numpy()
+Hardwood = fireData.HARD.to_numpy()
+Leaf = fireData.LEAF.to_numpy()
+Unknown = fireData.UNKNOWN.to_numpy()
+Other = fireData.OTHER.to_numpy()
+
+from sklearn.linear_model import SGDRegressor
