b76e166439b0135243c9a950346f252d78f4d901
Alex Johnson <aej800@gmail.com>
2025-10-28 10:30:03 -0800

    finished?

--- correlation.py
+++ correlation.py
@@ -5,28 +5,20 @@
 
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
+from operator import itemgetter
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import SGDRegressor
+from sklearn import metrics
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+from sklearn.neural_network import MLPClassifier
 
 input_file = "Mini Project Dataset Narrowed.csv"
 fireData = pd.DataFrame()
 fireData = pd.read_csv(input_file)
 
-
-# Continuous Variables
-Acres = fireData.ESTIMATEDTOTALACRES.to_numpy()
-
-# Discrete Ranges
-Slope = fireData.ORIGINSLOPE.to_numpy()
-Aspect = fireData.ORIGINASPECT.to_numpy()
-Elevation = fireData.ORIGINELEVATION.to_numpy()
-
-# Fuel/Vegetation Bools
-Spruce = fireData.SPRUCE.to_numpy()
-Tundra = fireData.MUNDRA.to_numpy() # moss and tundra included
-Grass = fireData.GRASS.to_numpy()
-Brush = fireData.BRUSH.to_numpy()
-Hardwood = fireData.HARD.to_numpy()
-Leaf = fireData.LEAF.to_numpy()
-Unknown = fireData.UNKNOWN.to_numpy()
-Other = fireData.OTHER.to_numpy()
-
-from sklearn.linear_model import SGDRegressor
